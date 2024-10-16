c3997c5c2f15ea36409acd9af1a2d62c2a15ffd5
Aniket <aniketkalawat88@gmail.com>
2024-10-16 11:16:49 +0530

    404-page-added

--- src/app/font.js
+++ src/app/font.js
@@ -1,10 +1,6 @@
-import { Inter, Montserrat , Londrina_Sketch , Poppins , Spinnaker} from "next/font/google";
+import { Montserrat , Londrina_Sketch } from "next/font/google";
 
 // If loading a variable font, you don't need to specify the font weight
-export const inter = Inter({
-    subsets: ["latin"],
-    display: "swap",
-  });
 
   export const montserrat = Montserrat({
     subsets: ["latin"],
@@ -16,8 +12,4 @@ export const inter = Inter({
     weight:['400']
   });
     
-  export const spinnaker = Spinnaker({
-    subsets: ["latin"],
-    weight:['400']
-  });
     
