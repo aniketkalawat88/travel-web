c31d6ba38f3dc6732e12efbefd270c069617ccb3
Aniket <aniketkalawat88@gmail.com>
2024-10-18 12:11:30 +0530

    number-change

--- src/app/_components/Whatsapp.jsx
+++ src/app/_components/Whatsapp.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Whatsapp() {
   return (
     <div>
-        <Link href="https://api.whatsapp.com/send?phone=+919630252729&text=Hey!%20Can%20I%20Get%20More%20Info%20On%20This?" target='_blank' className="fixed text-4xl right-[3vw] bottom-[3vw] z-30 max-sm:p-4">
+        <Link href="https://api.whatsapp.com/send?phone=++919425639221&text=Hey!%20Can%20I%20Get%20More%20Info%20On%20This?" target='_blank' className="fixed text-4xl right-[3vw] bottom-[3vw] z-30 max-sm:p-4">
             <div className="grid justify-items-center gap-2">
                 <div className="contact_icon ">
                     <img src="/assets/svg/WhatsApp.svg" alt="" className=" my-float h-12 w-12" />
